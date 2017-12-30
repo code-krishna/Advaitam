87872548eca46d4af7516c4bde9c4b2fba18e5b8
sourav8256 <souravmandalm@gmail.com>
2017-12-30 23:40:17 +0530

    Database implementation of participant list

--- app/src/main/java/Fragments/Place.java
+++ app/src/main/java/Fragments/Place.java
@@ -1,18 +1,40 @@
 package Fragments;
 
+import android.support.annotation.Keep;
+
+import java.io.Serializable;
+
 /**
  * Created by HP on 12/29/2017.
  */
 
-class Place {
-    public String nameOfStudent;
-    public String enroll;
-    public String phoneNo;
+@Keep
+public class Place {
+    public String nameStudent;
+    public String roll;
+    public String phone;
+
+    public Place(){
+
+    }
 
     public Place (String nameStudent, String roll, String phone)
     {
-        this.nameOfStudent = nameStudent;
-        this.enroll = roll;
-        this.phoneNo = phone;
+        this.nameStudent = nameStudent;
+        this.roll = roll;
+        this.phone = phone;
+    }
+
+
+    public String getNameStudent() {
+        return nameStudent;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 }
--- app/src/main/java/Fragments/PlaceArrayAdapter.java
+++ app/src/main/java/Fragments/PlaceArrayAdapter.java
@@ -1,12 +1,19 @@
 package Fragments;
 
+import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ac.nita.advaitam4.R;
 
@@ -14,30 +21,49 @@ import ac.nita.advaitam4.R;
  * Created by HP on 12/29/2017.
  */
 
-class PlaceArrayAdapter extends ArrayAdapter<Place> {
+class PlaceArrayAdapter extends BaseAdapter {
 
     Context mContext;
     int mLayoutResourceId;
-    Place mData[] = null;
+    //List<Place> mData = new ArrayList<Place>();
+    Place[] mData = null;
 
     public PlaceArrayAdapter(Context context, int row, Place[] myPlacesArray) {
-        super(context, row, myPlacesArray);
+        //super(context, row, myPlacesArray);
+
+        Log.d("mylog", "myplacesarray size is "+myPlacesArray.length);
+
         this.mContext = context;
         this.mLayoutResourceId = row;
         this.mData = myPlacesArray;
     }
+
     @Override
-    public Place getItem(int position)
-    {
-        return super.getItem(position);
+    public int getCount() {
+        return mData.length;
     }
+
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent)
     {
         View row = convertView;
         PlaceHolder holder = null;
+
         if(row==null){
-        LayoutInflater inflater = LayoutInflater.from(mContext);
+        //LayoutInflater inflater = LayoutInflater.from(mContext);
+            LayoutInflater inflater = (LayoutInflater) mContext
+                    .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
         row = inflater.inflate(mLayoutResourceId,parent,false);
         holder = new PlaceHolder();
         holder.nameView = (TextView) row.findViewById(ac.nita.advaitam4.R.id.rowTextView);
@@ -49,9 +75,10 @@ class PlaceArrayAdapter extends ArrayAdapter<Place> {
             holder = (PlaceHolder) row.getTag();
         }
         Place place = mData[position];
-        holder.nameView.setText(place.nameOfStudent);
-        holder.enroll.setText(place.enroll);
-        holder.phoneNo.setText(place.phoneNo);
+        Log.d("mylog", place.getNameStudent()+place.getPhone()+place.getRoll()+" ");
+        holder.nameView.setText(place.getNameStudent());
+        holder.enroll.setText(place.getRoll());
+        holder.phoneNo.setText(place.getPhone());
         return row;
     }
     private static class PlaceHolder {

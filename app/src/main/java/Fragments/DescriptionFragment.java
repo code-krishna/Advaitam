91c5ae8ac4f4063fbe291a397e4e4f4a77c3efd8
sourav8256 <souravmandalm@gmail.com>
2017-12-30 06:20:53 +0530

    Update

--- app/src/main/java/Fragments/DescriptionFragment.java
+++ app/src/main/java/Fragments/DescriptionFragment.java
@@ -1,19 +1,30 @@
 package Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import Info.CommonInfo;
 import Info.CulturalInfo;
 import Info.SportsInfo;
 import Info.TechnicalInfo;
 import ac.nita.advaitam4.Events;
 import ac.nita.advaitam4.MainActivity;
 import ac.nita.advaitam4.R;
+import ac.nita.advaitam4.TabbedActivity;
 
 /**
  * Created by HP on 06-08-2017.
@@ -24,6 +35,8 @@ public class DescriptionFragment extends Fragment {
     private String name;
     private String date;
     private String time;
+
+    String idName;
 
     public DescriptionFragment() {
     }
@@ -36,12 +49,14 @@ public class DescriptionFragment extends Fragment {
 	* 3. Return the rootView
 	* */
 
-	    String idName=getArguments().getString("KEY");
+        idName=getArguments().getString("KEY");
+        Log.d("mylog","Idname "+idName);
         View view=inflater.inflate(R.layout.fragment_description, container, false);
 
         char EventType=idName.charAt(0);
         int EventId=Character.getNumericValue(idName.charAt(idName.length()-1));
 
+        /*
         if(EventType=='s'){
             desc= SportsInfo.sInfo[EventId-1].getDesc();
             name= SportsInfo.sInfo[EventId-1].getName();
@@ -61,6 +76,10 @@ public class DescriptionFragment extends Fragment {
             time= TechnicalInfo.tInfo[EventId-1].getTime();
         }
 
+*/
+
+
+
         TextView tv=(TextView)view.findViewById(R.id.name);
         tv.setText("EVENT NAME: " + name);
         tv=(TextView)view.findViewById(R.id.desc);
@@ -77,4 +96,60 @@ public class DescriptionFragment extends Fragment {
         //return inflater.inflate(R.layout.fragment_description, container, false);
         return view;
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View v, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(v, savedInstanceState);
+
+        final View view = v;
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference reference = firebaseDatabase.getReference("data/events/"+idName);
+
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try {
+
+
+
+                    CommonInfo commonInfo = dataSnapshot.getValue(CommonInfo.class);
+                    desc= commonInfo.getDesc();
+                    name= commonInfo.getName();
+                    date= commonInfo.getDate();
+                    time= commonInfo.getTime();
+
+                    Log.d("mylog","Strings "+desc+name+date+time);
+
+                    TextView tv=(TextView)view.findViewById(R.id.name);
+                    tv.setText("EVENT NAME: \n" + name);
+                    tv=(TextView)view.findViewById(R.id.desc);
+                    tv.setText(desc);
+                    tv=(TextView)view.findViewById(R.id.time);
+                    tv.setText("TIME: " + time);
+                    tv=(TextView)view.findViewById(R.id.date);
+                    tv.setText("DATE: " + date);
+
+
+
+                } catch (Exception e){
+                    e=e;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
+
+
 }
